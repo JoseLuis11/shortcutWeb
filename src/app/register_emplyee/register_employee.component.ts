1c6f782cdf6ea52aecc1d4df182c22231e8cacf4
Emiliano <emiliano_rt@hotmail.com>
2018-04-21 20:38:52 -0700

    register employee components

---
+++ src/app/register_emplyee/register_employee.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register-employee',
+  templateUrl: './register_employee.component.html',
+})
+export class RegisterEmployeeComponent {
+  
+}
