05d31c6a2e1363b7a504f533ed391018f1d0ffda
Felipe Ortega Casillas <felipeorcas@gmail.com>
2018-04-21 20:47:45 -0700

    Creating owner homepage component

---
+++ src/app/onwer homepage/owner_homepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'owner-homepage',
+    tamplateUrl:'./owner_homepage.component.html',
+})
+
+export class OwnerHomepageComponent {
+    
+}
