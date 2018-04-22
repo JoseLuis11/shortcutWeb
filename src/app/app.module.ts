d8e96a69d018d66a9a01287d3006ba700225a815
José Luis Castro <jluis.scooter11@hotmail.com>
2018-04-21 22:33:23 -0700

    fixing firebase issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 //components
-import {OwnerHomepageComponent, RegisterEmployeeComponent, RegisterWorkplaceComponent, LoginOwnerComponent, RegisterOwnerComponent} from './index.pages'
+import { OwnerHomepageComponent, RegisterEmployeeComponent, RegisterWorkplaceComponent, LoginOwnerComponent, RegisterOwnerComponent } from './index.pages'
 import { AppComponent } from './app.component';
+
+//firebase
+import { AngularFireModule } from 'angularfire2';
+import { environment } from './../environments/environment';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
 @NgModule({
@@ -11,7 +18,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    RouterModule.forRoot([
+      { path: 'owner-home', component: OwnerHomepageComponent },
+      { path: 'register-employee', component: RegisterEmployeeComponent },
+      { path: 'register-workplace', component: RegisterWorkplaceComponent},
+      { path: 'login-owner', component: LoginOwnerComponent},
+      { path: 'register-owner', component: RegisterOwnerComponent },
+      { path: '', redirectTo: 'login-owner'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
