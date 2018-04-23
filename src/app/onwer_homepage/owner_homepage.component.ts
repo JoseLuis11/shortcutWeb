f1ee6ce3a27f2972d2e53a89470eff7b510296d5
José Luis Castro <jluis.scooter11@hotmail.com>
2018-04-22 23:33:48 -0700

    some changes

--- src/app/onwer_homepage/owner_homepage.component.ts
+++ src/app/onwer_homepage/owner_homepage.component.ts
@@ -1,10 +1,47 @@
+import { Workplace } from './../model/workplace.model';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'owner-homepage',
-    templateUrl:'./owner_homepage.component.html',
+    templateUrl: './owner_homepage.component.html',
 })
 
 export class OwnerHomepageComponent {
-    
+
+    ownerId: any;
+    workplaces: Observable<{}[]>;
+    filteredWorkplaces: any;
+    clients: any;
+    appointments: any;
+
+    constructor(private afAuth: AngularFireAuth, private afDb: AngularFireDatabase) {
+        this.afAuth.authState.subscribe(data => {
+            this.ownerId = data.uid;
+            console.log(data.uid);
+        });
+
+        this.loadWorkplaces();
+        this.loadAppointments();
+    }
+
+    loadWorkplaces() {
+        this.workplaces = this.afDb.list('workplaces').valueChanges();
+
+        this.filteredWorkplaces = this.workplaces.map(workplaces => {
+            return workplaces.filter((workplace: Workplace) => workplace.ownerId === this.ownerId);
+        });
+    }
+
+    loadAppointments(){
+        this.clients = this.afDb.list('clients').valueChanges();
+
+        this.clients.forEach(element => {
+            this.appointments.push(element.appointment);
+        });
+
+        console.log(this.appointments);
+    }
 }
